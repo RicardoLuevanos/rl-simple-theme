169aa80be40df912bf0b23dfd9d4aa225df1fafa
Rick Luevanos <rluevanos@gmail.com>
2016-05-11 20:57:08 -0700

    Refactors styles, cleans up unused snippets

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -29,27 +29,6 @@
 			if ( 'post' === get_post_type() ) : ?>
 			<div class="entry-meta">
 				<p>Published <?php rl_simple_theme_posted_on(); ?> in <?php rl_simple_theme_entry_footer(); ?></p>
-				<?php
-					$twitter_handle = get_theme_mod( 'twitter_handle_setting' );
-					$tweet_id = get_post_meta( $post->ID, 'tweet_id', true ); 
-					$short_url = urlencode( get_post_meta( $post->ID, 'short_url', true ) );
-
-					$tweet = urlencode( '"' . get_the_title( $post->ID ) . '"' );
-
-					if ( ! empty( $twitter_handle ) ) {
-						$tweet .= ' - @' . $twitter_handle; 
-					}
-
-					if ( ! empty( $short_url ) ) {
-						$tweet .= ', ' . $short_url; 
-					} else {
-						$tweet .= ', ' . get_permalink( $post->ID ); 
-					}
-
-					if ( count( wp_get_post_terms( $post->ID, 'post_tag' ) )  ) {
-						$tweet .= ', ' . urlencode( '#' . implode( ' #', wp_get_post_terms( $post->ID, 'post_tag', array( "fields" => "names" ) ) ) ); 
-					}
-				?>
 			</div><!-- .entry-meta -->
 			<?php
 			endif; 
